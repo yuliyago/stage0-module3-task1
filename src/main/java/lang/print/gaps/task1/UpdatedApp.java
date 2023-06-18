e180009ad02037865e4f7f14e539b5cc134e9889
yuliyago <yuuliya02@proton.me>
2023-06-19 00:37:11 +0300

    Five classes have been finished/fixed.

--- src/main/java/lang/print/gaps/task1/UpdatedApp.java
+++ src/main/java/lang/print/gaps/task1/UpdatedApp.java
@@ -1,7 +1,8 @@
 package lang.print.gaps.task1;
 
 public class UpdatedApp {
-    public static void main(String[] args) {
-        System.out.println("fixMe");
+    public static void main(String[] args)
+    {
+        System.out.println("I'm updated\ncan be compiled. And can write to console");
     }
 }
